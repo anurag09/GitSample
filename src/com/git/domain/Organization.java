10c54e9c45e2efa545b84f786971b21dbe70b94a
anurag09 <mishra.anurag09@gmail.com>
2015-06-05 18:16:43 +0530

    Added org domain

---
+++ src/com/git/domain/Organization.java
@@ -0,0 +1,31 @@
+package com.git.domain;
+
+/**
+ * Created by amm0924 on 6/5/2015.
+ */
+public class Organization {
+
+    private Long id;
+    private String name;
+
+    public Organization(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
